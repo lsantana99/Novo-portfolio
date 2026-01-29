6c54098ed3f6b549adfee3d68a990ddca6d5eeae
Lucas Santana de Oliveira <lucassantanadeoliveira_@hotmail.com>
2026-01-28 21:23:30 -0300

    Parte1
    Ajuste em margins
    JS toggle and contains em mostrar mais

---
+++ app.js
@@ -0,0 +1,7 @@
+let btnMore = document.querySelector('#more')
+
+btnMore.addEventListener('click', () =>{
+    let cardMore = document.querySelector('.cards-inviseble')
+
+    cardMore.style.display = 'block'
+})
