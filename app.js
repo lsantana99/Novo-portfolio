5f349de8c9a179f512db8d3086332c72b4352333
Lucas Santana de Oliveira <lucassantanadeoliveira_@hotmail.com>
2026-01-30 13:43:33 -0300

    Ajustes no Header Scrolled
    Ajustar cores no scrooll e animação no btn "Mostrar mais"
    
    Função no formulário para indicar direto ao Whatsapp

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let btnMore = document.querySelector('#more')
-let cardMore = document.querySelector('.cards-inviseble')
-
+let cardMore = document.querySelector('.cards-projects-extra')
+let header = document.querySelector('.header-desktop')
 
 btnMore.addEventListener('click', () =>{
-    const aberto = cardMore.classList.toggle('cards-inviseble')
+    const aberto = cardMore.classList.toggle('hidden')
     btnMore.textContent = aberto ? 'Mostrar mais' : 'Mostrar menos'
+})
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolled', window.scrollY > 50)
 })
